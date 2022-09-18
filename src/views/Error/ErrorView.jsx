8c607dca6e010aa3819e3d4e7072b34a6c8f7623
Hind <guessoushind@gmail.com>
2022-09-18 19:48:54 +0200

    Added 404 page

--- src/views/Error/ErrorView.jsx
+++ src/views/Error/ErrorView.jsx
@@ -1,12 +1,19 @@
 import React, { Fragment } from "react"
+import { Link } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import "./error.css"
 
 function ErrorView() {
 
     return (
         <Fragment>
             <Header />
+            <h3 className="error error--title">404</h3>
+            <h2 className="error error--subtitle">Oups! La page que vous demandez n'existe pas.</h2>
+            <Link to="/">
+                <p className="error error--return">Retourner sur la page d’accueil</p>
+            </Link>
             <Footer />
         </Fragment>
     )
