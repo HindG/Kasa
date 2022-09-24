05f270b22327f3dcc3784fd5f53bc75884ad5079
Hind <guessoushind@gmail.com>
2022-09-24 19:23:24 +0200

    Added gallery

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './thumb.css'
 
 class Thumb extends React.Component {
 
     render() {
-        const { accomodationTitle } = this.props;
+        const { accomodationData } = this.props;
+        const divStyle = {
+            background: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url('${accomodationData.cover}') center no-repeat`,
+            backgroundSize: "cover",
+            position: "relative",
+            width: "340px",
+            height: "340px",
+            margin: "25px",
+            borderRadius: "10px",
+        };
         return (
-            <div className="thumb">
-                <div className="thumb--title__container">
-                <span className="thumb--title">
-                    {accomodationTitle}
-                </span>
+            <Link key={accomodationData.id} to={`/accomodation/${accomodationData.id}`} className="thumb--link" >
+                <div style={divStyle}>
+                    <span className="thumb--title">
+                        {accomodationData.title}
+                    </span>
                 </div>
-            </div>
+            </Link >
         )
     }
 }
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,33 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState, useEffect } from "react"
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Thumb from "../../components/Thumb/Thumb";
+import Banner from "../../img/banner.png"
+import "./homepage.css";
 
 function HomePage() {
+    let [accomodationsData, setAccomodationsData] = useState(null)
+
+    useEffect(() => {
+        fetch("/logements.json")
+            .then(response => response.json())
+            .then(data => setAccomodationsData(data))
+    }, [])
 
     return (
         <Fragment>
             <Header homeView={true} />
+            <div className="banner--container">
+                <img src={Banner} alt="Banner" className="banner--img" />
+                <div className="banner--title">
+                    Chez vous, partout et ailleurs
+                </div>
+            </div>
+            {accomodationsData &&
+                <div className="accomodation--container">
+                 {accomodationsData.map((accomodation) => <Thumb accomodationData={accomodation} />)}
+                </div>
+            }
             <Footer />
         </Fragment>
     )
