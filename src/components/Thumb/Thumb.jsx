e4d3fcddafbefdb0dd3d898f9ec23a06a19c31e2
Hind <guessoushind@gmail.com>
2022-09-29 22:26:08 +0200

    Homepage responsive

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -8,16 +8,10 @@ class Thumb extends React.Component {
         const { accomodationData, index } = this.props;
         const divStyle = {
             background: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url('${accomodationData.cover}') center no-repeat`,
-            backgroundSize: "cover",
-            position: "relative",
-            width: "340px",
-            height: "340px",
-            margin: "25px",
-            borderRadius: "10px",
         };
         return (
             <Link to={`/accomodation/${accomodationData.id}`} className="thumb--link" >
-                <div key={index} style={divStyle}>
+                <div key={index} style={divStyle} className="thumb--picture">
                     <span className="thumb--title">
                         {accomodationData.title}
                     </span>
